4fd6099b8cf2e3e342f5e8a045c85cfe678155a3
Libo Cannici <lcannici@zendesk.com>
2015-01-24 22:47:37 +0100

    Dotted notation example

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def fibonacci(args={n: 10})
     Fibonacci.compute(args[:n])
   end
+
+  def user
+    User.new
+  end
 end
--- app/presenters/curly/index_presenter.rb
+++ app/presenters/curly/index_presenter.rb
@@ -2,4 +2,13 @@ class Curly::IndexPresenter < Curly::Presenter
   def fibonacci(n: '10')
     Fibonacci.compute(n)
   end
+
+  def user(identifier)
+    methods = identifier.split('.')
+    user = User.new
+
+    methods.inject(user) do |memo, m|
+      memo.public_send(m.to_sym)
+    end
+  end
 end
