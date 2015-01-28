5409db84fce59cc9f268b8deaec8c75cce8320d2
Libo Cannici <lcannici@zendesk.com>
2015-01-28 13:29:25 +0100

    Simple POC with PORO presenter in Handlebar

--- app/controllers/handlebars_controller.rb
+++ app/controllers/handlebars_controller.rb
@@ -1,4 +1,13 @@
 class HandlebarsController < ApplicationController
   def index
+    # presenter = Handlebars::IndexPresenter.new(self)
+
+    # I wish this was possible, it looks like it's not.
+    # Damn :...-(
+    # render(:index, presenter)
+
+    # This of course works, but it's a bit ugly...
+    text = FS::Template.new("app/views/handlebars/index.html.hbs").render(presenter)
+    render(text: text)
   end
 end
--- app/presenters/handlebars/index_presenter.rb
+++ app/presenters/handlebars/index_presenter.rb
@@ -1,10 +1,15 @@
-class Handlebars::IndexPresenter < Curly::Presenter
-  # If you need to assign variables to the presenter, you can use the
-  # `presents` method.
-  #
-  #   presents :foo, :bar
-  #
-  # Any public method defined in a presenter class will be available
-  # to the Curly template as a variable. Consider making these methods
-  # idempotent.
+require 'fibonacci'
+
+class Handlebars::IndexPresenter
+  def initialize(context)
+    @context = context
+  end
+
+  def fibonacci(args={n: 10})
+    Fibonacci.compute(args[:n])
+  end
+
+  def user
+    UserPresenter.new(@context.current_user)
+  end
 end
